0103e68214986fb1eafaa60905d8b2231c0e1b67
fihra <fabrofabian@gmail.com>
2023-04-25 22:07:07 -1000

    created Microphone class for instances

--- src/components/Microphone.js
+++ src/components/Microphone.js
@@ -1,6 +1,6 @@
 export const Microphone = (micName, micCost, micUrl) => {
     const name = micName;
-    const cost = "$" + micCost;
+    const cost = micCost;
     const url = micUrl;
     return {name, cost, url};
 }
