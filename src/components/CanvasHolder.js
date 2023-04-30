b3f628775e50aee66533525a3bfa077289c44109
fihra <fabrofabian@gmail.com>
2023-04-29 22:16:24 -1000

    renamed canvas to canvasHolder

--- src/components/CanvasHolder.js
+++ src/components/CanvasHolder.js
@@ -1,11 +1,15 @@
 import { useEffect, useRef, useContext } from 'react';
 import { PatternContext } from '../context/PatternContext';
 import usePattern from '../context/CanvasContext';
+import Canvas from 'react-responsive-canvas';
 
-const Canvas = (props) => {
+const CanvasHolder = (props) => {
     const canvasRef = useRef(null);
     const { currentPattern } = usePattern();
     const { windowSize } = props;
+
+    const desktopRadius = 50;
+    const mobileRadius = 50;
 
     const red = 'rgba(500, 0, 0, 0.5)';//Omnidirectional
     const orange = 'rgba(250, 190, 88, 0.5)';//Bidirectional
@@ -15,18 +19,18 @@ const Canvas = (props) => {
     const yellow = 'rgba(233, 212, 96, 0.5)';//Shotgun
     const brown = 'rgba(130, 94, 92, 0.5)';//Boundary
 
-    const drawOmnidirectional = (context) => {
+    const drawOmnidirectional = (context, canvas) => {
         //Omnidirectional
         context.strokeStyle = red;
         context.beginPath();
-        context.arc(350, 350, 296, 0, 2 * Math.PI);
+        context.arc(canvas.width/2, canvas.height/2, canvas.height/2 - 50, 0, 2 * Math.PI);
         context.fillStyle = red;
         context.fill();
         context.stroke();
         context.closePath();
     }
 
-    const drawBidirectional = (context) => {
+    const drawBidirectional = (context, canvas) => {
         //Bidirectional
         context.strokeStyle = orange;
         context.beginPath();
@@ -45,7 +49,7 @@ const Canvas = (props) => {
         context.closePath();
     }
 
-    const drawCardioid = (context) => {
+    const drawCardioid = (context, canvas) => {
         //Cardioid
         context.strokeStyle = green;
         context.beginPath();
@@ -72,7 +76,7 @@ const Canvas = (props) => {
         context.closePath();
     }
 
-    const drawHypercardioid = (context) => {
+    const drawHypercardioid = (context, canvas) => {
         //Hypercardioid
         context.strokeStyle = purple;
         context.beginPath();
@@ -107,7 +111,7 @@ const Canvas = (props) => {
         context.closePath();
     }
 
-    const drawSubcardioid = (context) => {
+    const drawSubcardioid = (context, canvas) => {
         //Subcardioid
         context.strokeStyle = blue;
         context.beginPath();
@@ -118,7 +122,7 @@ const Canvas = (props) => {
         context.closePath();
     }
 
-    const drawShotgun = (context) => {
+    const drawShotgun = (context, canvas) => {
         //Subcardioid
         context.strokeStyle = yellow;
         context.save();
@@ -166,7 +170,7 @@ const Canvas = (props) => {
         context.closePath();
     }
 
-    const drawBoundary = (context) => {
+    const drawBoundary = (context, canvas) => {
         //Boundary
         context.strokeStyle = brown;
         context.beginPath();
@@ -177,16 +181,17 @@ const Canvas = (props) => {
         context.closePath();
     }
 
-    const drawListener = (context) => {
+    const drawListener = (context, canvas) => {
         //Listener
         context.strokeStyle = 'black';
         context.beginPath();
-        context.arc(350, 350, 10, 0, 2 * Math.PI);
+        context.arc(canvas.width/2, canvas.height/2, 10, 0, 2 * Math.PI);
         context.stroke();
         context.closePath();
     }
 
     useEffect(() => {
+        // drawing();
         const canvas = canvasRef.current;
         if(!canvas){
             return;
@@ -197,47 +202,40 @@ const Canvas = (props) => {
             return;
         }
 
-        if(windowSize[0] < 940) {
-            canvas.scale = 0.5;
-        } else {
-            canvas.width = 700;
-            canvas.height = 700;
-        }
-
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         //Canvas
         context.strokeStyle = 'black';
         context.beginPath();
-        context.arc(350, 350, 300, 0, 2 * Math.PI);
+        context.arc(canvas.width/2, canvas.height/2, canvas.width/2 - 50, 0, 2 * Math.PI);
         context.stroke();
         context.closePath();
         
-        drawListener(context);
+        drawListener(context, canvas);
 
         switch(currentPattern) {
             case "Clear":
                 break;
             case "Omnidirectional":
-                drawOmnidirectional(context);
+                drawOmnidirectional(context, canvas);
                 break;
             case "Bidirectional":
-                drawBidirectional(context);
+                drawBidirectional(context, canvas);
                 break;
             case "Cardioid":
-                drawCardioid(context);
+                drawCardioid(context, canvas);
                 break;
             case "HyperCardioid":
-                drawHypercardioid(context);
+                drawHypercardioid(context, canvas);
                 break;
             case "SubCardioid":
-                drawSubcardioid(context);
+                drawSubcardioid(context, canvas);
                 break;
             case "Shotgun":
-                drawShotgun(context);
+                drawShotgun(context, canvas);
                 break;
             case "Boundary":
-                drawBoundary(context);
+                drawBoundary(context, canvas);
                 break;
             default:
                 return;
@@ -245,12 +243,25 @@ const Canvas = (props) => {
 
     }, [currentPattern, windowSize]);
 
+    const checkWindowSize = () => {
+        if(windowSize[0] < 940) {
+            return (
+                <canvas id="canvas" width="400px" height="400"
+                 ref={canvasRef}/>
+            )
+        } else {
+            return (
+                <canvas id="canvas" width="700px" height="700"
+                ref={canvasRef}/>
+            )
+        }
+    }
+
     return (
         <div className="canvas-container">
-            <canvas id="canvas" width="700px" height="700"
-            ref={canvasRef}/>
+            {checkWindowSize()}
         </div>
     )
 }
 
-export default Canvas;
+export default CanvasHolder;
