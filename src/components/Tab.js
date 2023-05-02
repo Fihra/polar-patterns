27119773ffa59ff47fdb38f88a8b5d5966c6ebcc
fihra <fabrofabian@gmail.com>
2023-05-01 14:19:05 -1000

    replaced panel with react-tooltip

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,47 +1,38 @@
 import React, { useState } from "react";
 import { diaphragmDefinitions } from "./DiaphragmData";
+import { Tooltip as ReactTooltip, Tooltip } from "react-tooltip";
 
 const Tab = (props) => {
-    const [panelActive, setPanelActive] = useState(false);
-    const [currentHover, setCurrentHover] = useState("");
+    const [currentMicInfo, setcurrentMicInfo] = useState("");
     const { size, data } = props;
-
-    const showPanel = () => {
-        setPanelActive(true);
-    }
-
-    const hidePanel = () => {
-        setPanelActive(false);
-        hideHover();
-    }
     
-    const showHover = (event) => {
-        setCurrentHover(event.currentTarget.dataset.info);
+    const showMicInfo = (event) => {
+        setcurrentMicInfo(event.currentTarget.dataset.info);
     }
 
-    const hideHover = () => {
-        setCurrentHover("");
+    const hideMicInfo = () => {
+        setcurrentMicInfo("");
     }
 
     return (
         <div className="tab-container">
             <section>
             <h3>{size}</h3>
-            {panelActive ? <div className="panel"><p>{diaphragmDefinitions[currentHover]}</p></div> : ""}
-            {Object.keys(data).map((item) => {
+            <Tooltip id="my-tooltip" />
+            {Object.keys(data).map((item, tooltip) => {
                 const original = item;
                 const words = item.split(/(?=[A-Z])/);
                 const firstLetter = words[0].charAt(0).toUpperCase();
                 const remainingLetters = words[0].substring(1);
                 words[0] = firstLetter + remainingLetters;
-
+                console.log(tooltip)
                 return (
                     <div >
-                        <div className="diaphragm-category" onMouseEnter={showPanel} onMouseOut={hidePanel} data-info={original} onMouseOver={showHover}>
+                        <div >
                             
-                            <p><b>{words.length > 1 ? words[0] + ' ' + words[1] : words[0]}:</b> {data[item]}</p>
+                            <p><b className="diaphragm-category" onMouseOut={hideMicInfo} data-info={original} onMouseOver={showMicInfo} data-tooltip-id="my-tooltip" data-tooltip-content={diaphragmDefinitions[currentMicInfo]}>{words.length > 1 ? words[0] + ' ' + words[1] : words[0]}:</b> {data[item]}</p>
                         </div>
-                    
+                        
                     </div>
                 )
             })}
