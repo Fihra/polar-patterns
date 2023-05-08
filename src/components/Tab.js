436076539fab384ba89f484d3b591d4d6894f8bd
fihra <fabrofabian@gmail.com>
2023-05-08 00:23:35 -1000

    created mobile panel state to manage the tooltip for the diaphragm size

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -4,7 +4,8 @@ import { Tooltip } from "react-tooltip";
 
 const Tab = (props) => {
     const [currentMicInfo, setcurrentMicInfo] = useState("");
-    const { size, data } = props;
+    const [mobilePanel, setMobilePanel] = useState(false);
+    const { size, data, windowSize } = props;
     
     const showMicInfo = (event) => {
         setcurrentMicInfo(event.currentTarget.dataset.info);
@@ -14,27 +15,14 @@ const Tab = (props) => {
         setcurrentMicInfo("");
     }
 
-    //TODO: Create mobile view to replace on hover effect of tooltips
-    // Create a panel on click event to open and close panel for mobile view only
-    const showMobileTooltip = () => {
-
-    }
-
-    const hideMobileTooltip = () => {
-
-    }
-
-    return (
-        <div className="tab-container">
-            <section>
-            <h3>{size}</h3>
-            <Tooltip id="my-tooltip"/>
-            {Object.keys(data).map((item, tooltip) => {
-                const original = item;
-                const words = item.split(/(?=[A-Z])/);
-                const firstLetter = words[0].charAt(0).toUpperCase();
-                const remainingLetters = words[0].substring(1);
-                words[0] = firstLetter + remainingLetters;
+    const showList = () => {
+        return Object.keys(data).map((item, tooltip) => {
+            const original = item;
+            const words = item.split(/(?=[A-Z])/);
+            const firstLetter = words[0].charAt(0).toUpperCase();
+            const remainingLetters = words[0].substring(1);
+            words[0] = firstLetter + remainingLetters;
+            if(windowSize[0] > 400){
                 return (
                     <div>
                         <div>
@@ -42,7 +30,41 @@ const Tab = (props) => {
                         </div>
                     </div>
                 )
-            })}
+            } else {
+                return (
+                    <div>
+                        <div style={{position: "relative"}}>
+                        {mobilePanel && item === currentMicInfo ? <div className="mobile-panel">{diaphragmDefinitions[currentMicInfo]}</div> : null}
+                            <p><b className="diaphragm-category" onClick={toggleMobileTooltip} data-info={original}>{words.length > 1 ? words[0] + ' ' + words[1] : words[0]}:</b> {data[item]}</p>
+                        </div>
+                    </div>
+                )
+            }
+
+        })
+    }
+
+    const toggleMobileTooltip = (e) => {
+        if(mobilePanel){
+            setMobilePanel(false);
+            hideMicInfo();
+        } else {
+            setMobilePanel(true);
+            setcurrentMicInfo(e.currentTarget.dataset.info);
+        }
+        console.log("show mobile panel");
+    }
+
+    const hideMobileTooltip = () => {
+        return 
+    }
+
+    return (
+        <div className="tab-container">
+            <section>
+            <h3>{size}</h3>
+            <Tooltip id="my-tooltip"/>
+            {showList()}
             </section>
         </div>
     )
