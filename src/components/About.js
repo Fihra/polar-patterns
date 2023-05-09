ead1e0dfd5bd8a42c65ed4e8457f50cfa9bead3b
fihra <fabrofabian@gmail.com>
2023-05-08 20:52:40 -1000

    paraphrased the about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
     return (
         <div className="about-container">
-            <p>A <b>polar pattern</b> is a representation of a microphone's directional sensitivity to sound pressure. In other words, polar patterns tell which direction(s) a mic will be sensitive to picking up sound and which direction(s) a mic will reject sound.</p>
+            <p>A <b>polar pattern</b> is a representation for a microphone's area of the audio signal input with the sound pressure. This displays the direction(s) of a mic in which it picks up sound and the direction(s) a mic will block sound.</p>
         </div>
     )
 }
